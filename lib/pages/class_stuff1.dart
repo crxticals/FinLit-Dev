eeb7dcda19cb95e4b3fb7f9fc2b9e6da255713e0
deezobdeez <alex.nguyen0201@icloud.com>
2024-10-29 13:33:33 -0700

    Added learning content from ChatGPT, to be replace later but this tests functionality.

--- lib/pages/class_stuff1.dart
+++ lib/pages/class_stuff1.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+// ignore: library_prefixes
 import 'package:flutter/services.dart' as rootBundle;
 
 class NameListScreen extends StatefulWidget {
@@ -21,8 +22,27 @@ class NameListScreenState extends State<NameListScreen> {
   }
 
   Future<void> loadNames() async {
-  final String response = await rootBundle.rootBundle.loadString('assets/Unit1_L1.json');
+  String fileName;
+
+  if (widget.index == 0) {
+    fileName = 'assets/Unit1_L1.json';
+  } else if (widget.index == 1) {
+    fileName = 'assets/Unit2_L2.json';
+  } else if (widget.index == 2) {
+    fileName = 'assets/Unit3_L3.json';
+  } else if (widget.index == 3) {
+    fileName = 'assets/Unit4_L4.json';
+  } else if (widget.index == 4) {
+    fileName = 'assets/Unit5_L5.json';
+  } else if (widget.index == 5) {
+    fileName = 'assets/Unit6_L6.json';
+  } else {
+    fileName = 'assets/default.json'; // default assignment, DO NOT DELETE PLZ
+  }
+
+  final String response = await rootBundle.rootBundle.loadString(fileName);
   final Map<String, dynamic> data = json.decode(response);
+
   setState(() {
     names = List<String>.from(data['lessons'].map((lesson) => lesson['lessonTitle']));
   });
@@ -32,9 +52,9 @@ class NameListScreenState extends State<NameListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lesson View - Unit ${widget.index + 1}'),
+        title: Text('$names - Unit ${widget.index + 1}'),
       ),
-      backgroundColor: const Color.fromRGBO(69, 80, 80, 1),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: ListView.builder(
         itemCount: names.length,
         itemBuilder: (context, index) {
