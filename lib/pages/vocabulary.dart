cebe342dc7d29866dfb9b80756498e3d7c6cfd94
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-12-06 12:46:26 -0500

    Fixed the vocabulary list
    This small commit is to fix the vocabulary list so it correctly renders the list of hard-coded vocabularies

--- lib/pages/vocabulary.dart
+++ lib/pages/vocabulary.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'vocab.dart';
+
+class Vocab {
+  final String word;
+  final String definition;
+
+  Vocab(this.word, this.definition);
+}
 
 class Vocabulary extends StatefulWidget {
   const Vocabulary({super.key});
@@ -10,74 +16,54 @@ class Vocabulary extends StatefulWidget {
 }
 
 class _VocabularyState extends State<Vocabulary> {
-
-    final _containerHeight = 700.0;
-    final ScrollController _scrollController = ScrollController();
-    int _currentScrollIndex = 0;
-  
-  @override
-  void initState(){
-    super.initState();
-
-    _scrollController.addListener(_updateScrollIndicator);
-  }
-
-  void _updateScrollIndicator(){
-    setState(() {
-      _currentScrollIndex = _scrollController.offset ~/ _containerHeight;
-    });
-  }
-
-  void _onNumberTap(int index){
-    _scrollController.animateTo(index * _containerHeight, 
-    duration: const Duration(seconds: 1), curve: Curves.easeIn);
-
-    setState(() {
-      _currentScrollIndex = index;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF3D3F6B),
-          title: const Text(
-            'Vocabulary',
-            style: 
-            TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-            )
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF3D3F6B),
+        title: const Text(
+          'Vocabulary',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
         ),
-        body: ListView.builder(
-            itemBuilder: (BuildContext context, int index) {
-            return ExpansionTile(
-                title: Text('Unit 1: The Time Value of Money'),
-                children: <Widget>[
-                    ExpansionTile(
-                      title: Text('1.1 Introduction of the Timeline '),
-                          children: <Widget>[
-                            ExpansionTile(title: Text(vocab[index].word),
-                            children: <Widget>[
-                              ListTile(title: Text(vocab[index].definition)),
-                            ]
-                            )
-                              ],
-                    ),
-                    ExpansionTile(
-                      title: Text('1.2 The three rules of time travel'),
-                      children: <Widget>[
-                        ListTile(title: Text('Item 3')),
-                        ListTile(title: Text('Item 4')),
-                      ],
-    ),
-  ],
-);
-          },
-          itemCount: vocab.length,
-        ),
-              );
+      ),
+      body: ExpansionTile(
+        title: const Text('Unit 1: The Time Value of Money'),
+        children: <Widget>[
+          ExpansionTile(
+            title: const Text('1.1 Introduction of the Timeline'),
+            children: <Widget>[
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: vocab.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(vocab[index].word),
+                    subtitle: Text(vocab[index].definition),
+                  );
+                },
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('1.2 The three rules of time travel'),
+            children: <Widget>[
+              const ListTile(title: Text('Item 3')),
+              const ListTile(title: Text('Item 4')),
+            ],
+          ),
+        ],
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    home: Vocabulary(),
+  ));
 }
